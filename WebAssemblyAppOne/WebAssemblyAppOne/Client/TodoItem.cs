8374df26d99fb63987ac1ab2872c6edf3925b517
edmondsylar <edmondmusiitwa@gmail.com>
2022-03-20 23:25:12 +0300

    App as a webAssembly

---
+++ WebAssemblyAppOne/WebAssemblyAppOne/Client/TodoItem.cs
@@ -0,0 +1,8 @@
+public class TodoItem
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? creator { get; set; }
+    public bool? complete { get; set; }
+
+}
